216ecfa4b099992cc0ebe936d3fb1c99d86ebcbe
PranjalbaZala <65394646+PranjalbaZala@users.noreply.github.com>
2021-11-12 15:21:57 +0530

    Add files via upload

---
+++ RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace OnlineBookStoreMVC
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Book", action = "Book", id = UrlParameter.Optional } //Entry point of program
+            );
+        }
+    }
+}
